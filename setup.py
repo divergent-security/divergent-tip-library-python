b4345fae68e3399739e14137754a94f43fa0354e
cneckar <cneckar@divergentsecurity.com>
2019-04-27 11:59:09 -0600

    get ready for pip

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(name='divergent_tip_job_library',
       version='0.1',
-      description="Job Template's for Divergent Security's Threat Intelligence Portal",
+      description="Python Job Library for Divergent Security's Threat Intelligence Platform",
       url='https://tip.divergentsecurity.com',
       author='Divergent Security',
       author_email='support@divergentsecurity.com',
-      license='MIT',
-      packages=[ 'divergent_tip_job_library' ],
+      license='None',
+      packages=[ 'divergent_tip_library_python' ],
       install_requires=[
-          'netaddr'
+          'netaddr',
+          'urlparse'
       ],
       zip_safe=False)
